56391e599cae1111db972e68d21577dda0becd00
arthuralvesdl <arthuralveslacerda@gmail.com>
2025-01-07 20:26:04 -0300

    chore: add comment

--- dicionario_mobile_codesh/lib/app/provider/app_provider.dart
+++ dicionario_mobile_codesh/lib/app/provider/app_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 
 class AppProvider extends ChangeNotifier {
-    // PROVIDER CRIADO PARA GERENCIAR O APP COMO UM TOD0
-
+  // PROVIDER CRIADO PARA GERENCIAR O APP COMO UM TOD0
+  /*
+      para um app de dicionario, aqui seria interessante adicionar a multilinguagem para o usuário escolher o idioma
+     */
 }
